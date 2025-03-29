cbe4bcf46ca541b0e7b830196efaad353db35dee
b.beseda <b.beseda@webvork.com>
2025-03-29 16:04:34 +0300

    fix

--- backend/routes/cargo.py
+++ backend/routes/cargo.py
@@ -1,42 +1,41 @@
 from fastapi import APIRouter, Depends, HTTPException
-from sqlalchemy.orm import Session
-# import crud, schemas
+from sqlalchemy.ext.asyncio import AsyncSession
+import crud.cargo_crud as crud
 import schemas
-from crud.cargo_crud import create_cargo, get_cargo, get_cargos, update_cargo, delete_cargo
 from database import get_db
 
-router = APIRouter()
+router = APIRouter(prefix="/cargo", tags=["Cargo"])
 
 
 @router.post("/", response_model=schemas.Cargo)
-def add_cargo(cargo: schemas.CargoCreate, db: Session = Depends(get_db)):
-    return create_cargo(db=db, cargo=cargo)
+async def create_cargo(cargo: schemas.CargoCreate, db: AsyncSession = Depends(get_db)):
+    return await crud.create_cargo(db=db, cargo=cargo)
 
 
 @router.get("/{cargo_id}", response_model=schemas.Cargo)
-def read_cargo(cargo_id: int, db: Session = Depends(get_db)):
-    cargo = get_cargo(db=db, cargo_id=cargo_id)
+async def read_cargo(cargo_id: int, db: AsyncSession = Depends(get_db)):
+    cargo = await crud.get_cargo(db=db, cargo_id=cargo_id)
     if cargo is None:
         raise HTTPException(status_code=404, detail="Cargo not found")
     return cargo
 
 
 @router.get("/", response_model=list[schemas.Cargo])
-def read_cargos(skip: int = 0, limit: int = 10, db: Session = Depends(get_db)):
-    return get_cargos(db=db, skip=skip, limit=limit)
+async def read_cargos(skip: int = 0, limit: int = 10, db: AsyncSession = Depends(get_db)):
+    return await crud.get_cargos(db=db, skip=skip, limit=limit)
 
 
 @router.put("/{cargo_id}", response_model=schemas.Cargo)
-def edit_cargo(cargo_id: int, cargo_update: schemas.CargoUpdate, db: Session = Depends(get_db)):
-    cargo = update_cargo(db=db, cargo_id=cargo_id, cargo_update=cargo_update)
+async def update_cargo(cargo_id: int, cargo_update: schemas.CargoUpdate, db: AsyncSession = Depends(get_db)):
+    cargo = await crud.update_cargo(db=db, cargo_id=cargo_id, cargo_update=cargo_update)
     if cargo is None:
         raise HTTPException(status_code=404, detail="Cargo not found")
     return cargo
 
 
 @router.delete("/{cargo_id}", response_model=schemas.Cargo)
-def delete_cargo_endpoint(cargo_id: int, db: Session = Depends(get_db)):
-    cargo = delete_cargo(db=db, cargo_id=cargo_id)
+async def delete_cargo(cargo_id: int, db: AsyncSession = Depends(get_db)):
+    cargo = await crud.delete_cargo(db=db, cargo_id=cargo_id)
     if cargo is None:
         raise HTTPException(status_code=404, detail="Cargo not found")
     return cargo
