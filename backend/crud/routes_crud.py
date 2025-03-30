97a3d4e423c3d19741c83f032ec0b93af7c6fd04
b.beseda <b.beseda@webvork.com>
2025-03-30 16:21:40 +0300

    com

--- backend/crud/routes_crud.py
+++ backend/crud/routes_crud.py
@@ -1,20 +1,26 @@
 from sqlalchemy.ext.asyncio import AsyncSession
-from models import Route as RouteModel
+from sqlalchemy.future import select
+from models import Route
 from schemas import RouteCreate
 from fastapi import HTTPException
 
 async def create_route(db: AsyncSession, route_data: RouteCreate):
-    new_route = RouteModel(**route_data.dict())
+    new_route = Route(**route_data.dict())
     db.add(new_route)
     await db.commit()
     await db.refresh(new_route)
     return new_route
 
 async def get_route(db: AsyncSession, route_id: int):
-    route = await db.get(RouteModel, route_id)
+    route = await db.get(Route, route_id)
     if route is None:
         raise HTTPException(status_code=404, detail="Route not found")
     return route
+
+async def get_routes(db: AsyncSession, offset: int = 0, limit: int = 10):
+    result = await db.execute(select(Route).order_by(Route.id.desc()).offset(offset).limit(limit))
+    routes = result.scalars().all()
+    return routes
 
 async def update_route(db: AsyncSession, route_id: int, route_data: RouteCreate):
     route = await get_route(db, route_id)
--- backend/routes/routes.py
+++ backend/routes/routes.py
@@ -3,13 +3,18 @@ from sqlalchemy.ext.asyncio import AsyncSession
 
 from database import get_db
 from schemas import RouteCreate, Route
-from crud.routes_crud import create_route, get_route, update_route, delete_route
+from crud.routes_crud import create_route, get_route, get_routes, update_route, delete_route
 
 router = APIRouter()
 
 @router.post("/", response_model=Route)
 async def create_route_endpoint(route_data: RouteCreate, db: AsyncSession = Depends(get_db)):
     return await create_route(db, route_data)
+
+
+@router.get("/", response_model=Route)
+async def get_routes(route_id: int, db: AsyncSession = Depends(get_db)):
+    return await get_routes(db, route_id)
 
 @router.get("/{route_id}", response_model=Route)
 async def get_route_endpoint(route_id: int, db: AsyncSession = Depends(get_db)):
